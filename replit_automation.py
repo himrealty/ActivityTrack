6a6536c92dfb4b72a85573b3acec7e6952b6abe0
himrealty <testhimrealty@gmail.com>
2025-12-16 14:03:16 +0530

    Update replit_automation.py

--- replit_automation.py
+++ replit_automation.py
@@ -17,7 +17,8 @@ print('=' * 80)
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-# chrome_options.add_argument('--headless')  # COMMENTED OUT FOR DEBUGGING
+chrome_options.add_argument('--headless')  # REQUIRED for GitHub Actions
+chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--window-size=1920,1080')
 chrome_options.add_argument('--disable-blink-features=AutomationControlled')
 chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
